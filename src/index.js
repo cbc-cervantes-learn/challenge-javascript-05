caf9d667e292adfc3c94e37e34db72abd280d63c
Cristian Bello Cervantes <cristiancervantes.cbc@gmail.com>
2020-06-12 20:35:23 -0500

    Add challenge solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@
  */
 
 const pascalTriangle = (lineNumber) => {
-  
-}
+    const lines = [];
+    for(let index = 0; index <= lineNumber ; index++) {
+        let line = [];
+        if (index === 0){
+            line.push(1);
+        }else{
+            let numbers = 0;
+            while (numbers < index + 1){
+                const prev_line = lines[index - 1];
+                let left_number = prev_line[numbers - 1] === undefined ? 0 : prev_line[numbers - 1];
+                let right_number = prev_line[numbers] === undefined ? 0 : prev_line[numbers];
+                let number = left_number + right_number;
+                line.push(number);
+                numbers ++;
+            }
+        }
+        lines.push(line);
+    }
+    return lines[lineNumber];
+};
 
 module.exports = pascalTriangle;
